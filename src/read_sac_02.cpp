1106f1cb1d90ac04575e317ced28762ae4f32117
arbCoding <arbCoding@gmail.com>
2023-04-12 06:50:53 -0700

    Added in reading multiple words

--- src/read_sac_02.cpp
+++ src/read_sac_02.cpp
@@ -11,11 +11,14 @@ static constexpr int word_length{4};
 // There is no converting to the correct format (string, integer, float)
 // Nor is the word being returned to the caller so that it can be passed
 // elsewhere
+
 // This is "pass by pointer" (hence the * and the ->)
 std::array<char, word_length> read_next_word(std::ifstream* sac)
 {
   // If it isn't defined as static, it cannot be returned (will be destroyed on end of function)
   char word[word_length] = {};
+  // C-like cast, needs to be re-done with a correct form
+  // static_cast<char*>(&word) doesn't work
   sac->read((char*) &word, word_length);
   std::array<char, word_length> result;
   for (int i{0}; i < word_length; ++i)
@@ -25,9 +28,53 @@ std::array<char, word_length> read_next_word(std::ifstream* sac)
   return result;
 }
 
+// Read in multiple words
+// For some reason if I use int instead of longusigned int it fails to compile (sign conversion error)
+// This happens even when I provide it with an int as input
+// As it'll never be negative anyway, I deem it okay to use long unsigned int in this case
+template <long unsigned int N> // template for array size
+std::array<char, N> read_words(std::ifstream* sac, int n_words = 1)
+{
+  std::array<char, word_length> single_word;
+  std::array<char, N> full_value;
+  int destination_start{0};
+  for (int words{0}; words < n_words; ++words)
+  {
+    single_word = read_next_word(sac);
+    destination_start = words * word_length;
+    std::copy(single_word.begin(), single_word.end(), full_value.begin() + destination_start);
+  }
+  return full_value;
+}
+
 int word_position(int word_number)
 {
   return (word_number * word_length);
+}
+
+template <long unsigned int N>
+void print_array(std::array<char, N> input_array)
+{
+  for (long unsigned int i{0}; i < sizeof(input_array); i++)
+  {
+    std::cout << input_array[i] << '\t' << static_cast<int>(input_array[i]) << '\n';
+  }
+}
+
+// This will read a single word and spit out the raw contents
+void test_read(std::ifstream* sac)
+{
+  std::array<char, word_length> test;
+  test = read_next_word(sac);
+  print_array(test);
+}
+
+// This will read in multiple words (2 of them) and spit out the raw contents
+void test_read_two(std::ifstream* sac)
+{
+  std::array<char, 2 * word_length> test;
+  test = read_words<sizeof(test)>(sac, sizeof(test));
+  print_array(test);
 }
 
 // argc is the argument count, 
@@ -45,30 +92,29 @@ int main(int argc, char* argv[])
     return 1;
   }
   std::ifstream sac_file(argv[1], std::ifstream::binary);
-  //std::ifstream sac_file("./data/IM.NV31..BHZ.M.2023.094.222304.SAC", std::ifstream::binary);
+
   if (!sac_file)
   {
     std::cerr << "File could not be read...\n";
     return 1;
   }
-  sac_file.seekg(word_position(110)); // KSTNM is 2 words (8 characters long)
-  std::array<char, word_length> test;
-  test = read_next_word(&sac_file);
-  //test = read_next_word(&sac_file);
-  for (long unsigned int i{0}; i < sizeof(test); ++i)
-  {
-    std::cout << test[i] << '\t' << static_cast<int>(test[i]) <<  '\n';
-  }
-  std::cout << '\n';
 
-  // It works! We can keep reading subsequent words!
-  // Next is to defined a funciton that will take an argument for the
-  // number of words to read and return that as a character array
-  test = read_next_word(&sac_file);
-  for (long unsigned int i{0}; i < sizeof(test); ++i)
-  {
-    std::cout << test[i] << '\t' << static_cast<int>(test[i]) << '\n';
-  }
+  sac_file.seekg(word_position(110)); // KSTNM is 2 words (8 characters long)
+  // Reading in a single word works!
+  test_read(&sac_file);
+  test_read(&sac_file);
+  std::cout << '\n' << '\n';
+  sac_file.seekg(word_position(110));
+  // Reading in two words works!
+  test_read_two(&sac_file);
+  std::cout << '\n' << '\n';
+  // What about reading in 4 words?
+  // Yes it works perfectly!
+  // Next I need a function to check for the special value of -12345 (unset in SAC)
+  sac_file.seekg(word_position(110));
+  std::array<char, 4 * word_length> test;
+  test = read_words<sizeof(test)>(&sac_file, sizeof(test));
+  print_array(test);
 
   return 0;
 }
