d1569443e05c2a4e5c076aaa6e23938a0a00e91b
Alexander Blanchette <arbCoding@gmail.com>
2023-11-17 13:22:29 -0800

    Updated build script

--- build_docs.sh
+++ build_docs.sh
@@ -22,7 +22,15 @@ rm *.tex *.pdf
 # Compress and rename pdf
 cd $base/docs
 echo "\nCompressing pdf"
+orig_size=`ls -l | grep index | grep pdf | awk '{print $5}'`
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dNOPAUSE -dQUIET -dBATCH -dPrinted=false -sOutputFile=index-compressed.pdf index.pdf 
 mv index-compressed.pdf sac-format_manual.pdf
 rm index.pdf
+new_size=`ls -l | grep manual | grep pdf | awk '{print $5}'`
+
+percent=`echo 'scale=1; ('"$new_size"'/'"$orig_size"') * 100' | bc`
+orig_size=`echo 'scale=0; '"$orig_size"' / 1000.0' | bc`
+new_size=`echo 'scale=0; '"$new_size"' / 1000.0' | bc`
+
+echo "($orig_size kB, $new_size kB, $percent%)\n"
 cd $base
