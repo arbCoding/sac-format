403557ae3fd724e87af84b1c119b829f37847611
Alexander Blanchette <arbCoding@gmail.com>
2023-11-17 10:38:03 -0800

    Fixed link for docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,14 +1,23 @@
 #!/bin/dash
 # Clean up old files
+base=`pwd`
 echo "Cleaning up past build!"
 rm -r ./docs/*
 
 # Build with emacs
 echo "Building with emacs!"
-emacs -Q --script ./build_site.el
+emacs -Q --script ./build_docs.el
 
 # Time to shrink the files!
 echo "Minify-ing files!"
-cd ./docs/
+cd $base/docs/
 ls -1 | grep html | parallel 'minify {} -o {}.new; mv {}.new {}'
-cd ../
+cd $base
+
+# Cleanup
+cd $base/src/docs
+rm *.tex *.pdf
+
+cd $base/docs
+mv index.pdf sac-format_manual.pdf
+cd $base
