b964b0e5a7a35b9ccf539d512b0f10cb1ad48a58
Alexander Blanchette <arbCoding@gmail.com>
2023-11-19 17:17:51 -0800

    Doing code coverage locally while I figure out building with CMakePresets.json on runners

--- get_coverage.sh
+++ get_coverage.sh
@@ -13,11 +13,12 @@ ctest
 # Locate the gcov output
 cd ./CMakeFiles/sac-format.dir/
 # Run lcov to generate coverage report
-lcov -c -d . -o sac-format.info
+lcov -c -d . -o sac-format.txt
 # Remove standard library references (linux)
 lcov -r sac-format.txt "/usr*" -o sac-format.txt
 # Convert to html
 genhtml --demangle-cpp sac-format.txt -o coverage --num-spaces 4
+cp sac-format.txt $base/sac-format.txt
 # Move the coverage files
 mv ./coverage $base/coverage
 cd $base
