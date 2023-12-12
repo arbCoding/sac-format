7f38ab68597f32d4f00866287a5a24e63cf376f2
Alexander Blanchette <arbCoding@gmail.com>
2023-12-12 06:37:27 -0800

    Tweaks to appearance of license file in installers.

--- scripts/productbuild.sh
+++ scripts/productbuild.sh
@@ -8,6 +8,7 @@ if [ -e "$base/build/release/clang" ]; then
     rm -rf "$base/build/release/clang"
 fi
 cp "$base/LICENSE" "$base/License.txt"
+fold -s "$base/LICENSE" > "$base/License2.txt"
 # Prep the build
 cmake -DCPACK_GENERATOR=productbuild \
     -DCMAKE_INSTALL_PREFIX=/opt/sac-format \
@@ -20,6 +21,6 @@ cpack -G productbuild
 # Move the package
 mv ./sac-format.pkg "$base/sac-format.pkg"
 # Cleanup
-rm "$base/License.txt"
+rm "$base/License.txt" "$base/License2.txt"
 # Return home
 cd "$script" || exit
